36f2a23152987a8f0ce75d2c68f4ff8197d55e04
Truong Minh Duc <truongducvt123@gmail.com>
2023-06-19 22:22:54 +0700

    feat: Add repository

--- Repository/Repository/RepositoryBase.cs
+++ Repository/Repository/RepositoryBase.cs
@@ -1,12 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Repository.Models;
 
-namespace Repository.Repository
+namespace NoteTakingApp.Repository
 {
-    internal class RepositoryBase
+    public class RepositoryBase<T> where T : class
     {
+        NoteTakingAppContext _context;
+        DbSet<T> _dbSet;
+
+        public RepositoryBase()
+        {
+            _context = new NoteTakingAppContext();
+            _dbSet = _context.Set<T>();
+        }
+
+        public List<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public void Create(T entity)
+        {
+            _dbSet.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public Boolean Delete(T entity)
+        {
+            try
+            {
+                _dbSet.Remove(entity);
+                _context.SaveChanges(true);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+        public void Update(T entity)
+        {
+            var tracker = _context.Attach(entity);
+            tracker.State = EntityState.Modified;
+            _context.SaveChanges(true);
+        }
+
+
     }
 }
