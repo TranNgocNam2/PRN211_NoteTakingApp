0acf4698cc2b03b424a82e4603fe1826f26f56fe
Truong Minh Duc <truongducvt123@gmail.com>
2023-06-22 20:38:57 +0700

    feat: add create note

---
+++ NoteTakingApp/Forms/NoteEditor.cs
@@ -0,0 +1,45 @@
+﻿using Repository.Models;
+using Repository.Repository;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NoteTakingApp.Forms
+{
+    public partial class NoteEditor : Form
+    {
+        NoteRepository noteRepository;
+
+        public NoteEditor()
+        {
+            InitializeComponent();
+            noteRepository = new NoteRepository();
+        }
+
+        private void btnApply_Click(object sender, EventArgs e)
+        {
+            String noteTitle = txtTitle.Text;
+            String noteContent = txtContent.Text;
+            Note note = new Note()
+            {
+                UserId = 1,
+                Title = noteTitle,
+                Content = noteContent,
+                CreatedAt = DateTime.Now
+            };
+            noteRepository.Create(note);
+            this.Hide();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
